c7b812a37bdffcf77904671212a4a5c9ea1beafa
Benjamin Bengfort <benjamin@bengfort.com>
2016-05-28 21:49:25 -0700

    timing

---
+++ notebooks/tutorial/bryde/__init__.py
@@ -0,0 +1,18 @@
+# bryde
+# Small utilities for the NLTK tutorial
+#
+# Author:   Benjamin Bengfort <bbengfort@districtdatalabs.com>
+# Created:  Sat May 28 21:42:44 2016 -0700
+#
+# Copyright (C) 2016 District Data Labs
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+Small utilities for the NLTK tutorial
+"""
+
+##########################################################################
+## Imports
+##########################################################################
